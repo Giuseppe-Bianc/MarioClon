f0c56d2a7fddce2d329df6b1314c208e93e8d9c7
Giuseppe-Bianc <75339264+Giuseppe-Bianc@users.noreply.github.com>
2022-09-05 11:56:33 +0200

    the window is up on the screen

--- src/main/java/org/dersbian/jade/Window.java
+++ src/main/java/org/dersbian/jade/Window.java
@@ -1,4 +1,83 @@
 package org.dersbian.jade;
 
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
 public class Window {
+	private int width, height;
+	private String title;
+	private long glfwWindow;
+
+	private static  Window window = null;
+	private Window() {
+		this.width = 960;
+		this.height = 540;
+		this.title = "MarioClon";
+	}
+
+	public static Window get(){
+		if(Window.window == null){
+			Window.window = new Window();
+		}
+		return Window.window;
+	}
+
+	public void run() {
+		System.out.println("Hello LWJGL " + org.lwjgl.Version.getVersion() + "!");
+
+		init();
+		loop();
+
+		glfwFreeCallbacks(glfwWindow);
+		glfwDestroyWindow(glfwWindow);
+
+		glfwTerminate();
+		java.util.Objects.requireNonNull(glfwSetErrorCallback(null)).free();
+	}
+
+	private void init() {
+		GLFWErrorCallback.createPrint(System.err).set();
+
+		if ( !glfwInit() )
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+		glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+		if ( glfwWindow == NULL )
+			throw new RuntimeException("Failed to create the GLFW window");
+
+
+		glfwMakeContextCurrent(glfwWindow);
+		glfwSwapInterval(1);
+
+		glfwShowWindow(glfwWindow);
+
+		GL.createCapabilities();
+	}
+
+	private void loop() {
+
+
+		while ( !glfwWindowShouldClose(glfwWindow) ) {
+			glfwPollEvents();
+
+			glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+			glfwSwapBuffers(glfwWindow); // swap the color buffers
+
+		}
+	}
 }
